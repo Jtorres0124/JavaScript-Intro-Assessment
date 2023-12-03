698c17ec6a0c235481c00665cd3a72178a1256f9
Jonathan Torres <codevondri@gmail.com>
2023-12-03 18:47:11 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// Declare variables
+var firstName, lastName, age;
+
+// Assign values to variables
+firstName = "Jonathan";
+lastName = "Torres";
+age = 27; // Replace with your actual age or a fake age
+
+// Fire an alert with the full name
+alert("My name is " + firstName + " " + lastName);
+
+// Fire an alert with the age
+alert("I am " + age + " years old");
+
+// Log a message in the console
+console.log("I am having so much fun in the class!");
